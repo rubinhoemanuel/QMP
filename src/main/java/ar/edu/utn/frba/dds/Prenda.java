73b91d758d8faf392c8f69ba238a1174dd34434a
Emanuel Rubiño <rubinhoemanuel@gmail.com>
2024-04-14 12:22:53 -0300

    Se agregar validacion en la prenda con excepciones y se cambia DER y test

--- src/main/java/ar/edu/utn/frba/dds/Prenda.java
+++ src/main/java/ar/edu/utn/frba/dds/Prenda.java
@@ -9,11 +9,30 @@ public class Prenda {
     private Color colorSecundario;
 
     public Prenda(TipoPrenda tipo, CategoriaPrenda categoria, Material material, Color colorPrincipal, Color colorSecundario) {
+        validar(tipo, categoria, material, colorPrincipal, colorSecundario);
         this.tipo = tipo;
         this.categoria = categoria;
         this.material = material;
         this.colorPrincipal = colorPrincipal;
         this.colorSecundario = colorSecundario;
+    }
+
+    private void validar(TipoPrenda tipo, CategoriaPrenda categoria, Material material, Color colorPrincipal, Color colorSecundario) {
+        if (tipo==null) {
+            throw new RuntimeException("No se cargo el tipo de prenda");
+        }
+        if (categoria==null) {
+            throw new RuntimeException("No se cargo la categoria de la prenda");
+        }
+        if (material==null) {
+            throw new RuntimeException("No se cargo el material de la prenda");
+        }
+        if (colorPrincipal==null) {
+            throw new RuntimeException("No se cargo el color primario de la prenda");
+        }
+        if (tipo.getCategoria() != categoria) {
+            throw new RuntimeException("La categoria del tipo de prenda no se condice con la categoria de la prenda");
+        }
     }
 
     public boolean existeColorSecundario() {
--- src/test/java/ar/edu/utn/frba/dds/PrendaTest.java
+++ src/test/java/ar/edu/utn/frba/dds/PrendaTest.java
@@ -7,14 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PrendaTest {
 
-    private FabricaDePrendas fabricaDePrendas;
     private TipoPrenda tipoPrendaRemera;
     private Color colorNegro;
     private Color colorBlanco;
 
     @BeforeEach
     public void init() {
-        fabricaDePrendas = new FabricaDePrendas();
         tipoPrendaRemera = new TipoPrenda("Nike", CategoriaPrenda.PARTE_SUPERIOR);
         colorNegro = new Color(0,0,0);
         colorBlanco = new Color(255,255,255);
@@ -23,7 +21,7 @@ public class PrendaTest {
     @Test
     public void crearPrendaSinTipo() {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            Prenda prendaSinTipo = fabricaDePrendas.crearPrenda(null, null, null, null, null);
+            Prenda prendaSinTipo = new Prenda(null, null, null, null, null);
         });
         String expectedMessage = "No se cargo el tipo de prenda";
         String actualMessage = exception.getMessage();
@@ -33,7 +31,7 @@ public class PrendaTest {
     @Test
     public void crearPrendaSinCategoria() {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            Prenda prendaSinCategoria = fabricaDePrendas.crearPrenda(tipoPrendaRemera, null, null, null, null);
+            Prenda prendaSinCategoria = new Prenda(tipoPrendaRemera, null, null, null, null);
         });
         String expectedMessage = "No se cargo la categoria de la prenda";
         String actualMessage = exception.getMessage();
@@ -43,7 +41,7 @@ public class PrendaTest {
     @Test
     public void crearPrendaSinMaterial() {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            Prenda prendaSinMaterial = fabricaDePrendas.crearPrenda(tipoPrendaRemera, CategoriaPrenda.PARTE_SUPERIOR, null, null, null);
+            Prenda prendaSinMaterial = new Prenda(tipoPrendaRemera, CategoriaPrenda.PARTE_SUPERIOR, null, null, null);
         });
         String expectedMessage = "No se cargo el material de la prenda";
         String actualMessage = exception.getMessage();
@@ -53,7 +51,7 @@ public class PrendaTest {
     @Test
     public void crearPrendaSinColorPrimario() {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            Prenda prendaSinColorPrimario = fabricaDePrendas.crearPrenda(tipoPrendaRemera, CategoriaPrenda.PARTE_SUPERIOR, Material.ALGODON, null, null);
+            Prenda prendaSinColorPrimario = new Prenda(tipoPrendaRemera, CategoriaPrenda.PARTE_SUPERIOR, Material.ALGODON, null, null);
         });
         String expectedMessage = "No se cargo el color primario de la prenda";
         String actualMessage = exception.getMessage();
@@ -63,7 +61,7 @@ public class PrendaTest {
     @Test
     public void crearPrendaSinCoincidirCategoriaTipoPrendaConCategoria() {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            Prenda prendaSinColorPrimario = fabricaDePrendas.crearPrenda(tipoPrendaRemera, CategoriaPrenda.CALZADO, Material.ALGODON, colorNegro, null);
+            Prenda prendaSinColorPrimario = new Prenda(tipoPrendaRemera, CategoriaPrenda.CALZADO, Material.ALGODON, colorNegro, null);
         });
         String expectedMessage = "La categoria del tipo de prenda no se condice con la categoria de la prenda";
         String actualMessage = exception.getMessage();
@@ -72,7 +70,7 @@ public class PrendaTest {
 
     @Test
     public void verificarColorSecundarioPrenda() {
-        Prenda prendaConColorSecundario = fabricaDePrendas.crearPrenda(tipoPrendaRemera, CategoriaPrenda.PARTE_SUPERIOR, Material.ALGODON, colorNegro, colorBlanco);
+        Prenda prendaConColorSecundario = new Prenda(tipoPrendaRemera, CategoriaPrenda.PARTE_SUPERIOR, Material.ALGODON, colorNegro, colorBlanco);
         assertTrue(prendaConColorSecundario.existeColorSecundario());
     }
 
